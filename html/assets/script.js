d3f048454826419461685e8db2c36f0534d8a65d
Josh Yudaken <j@yud.co.za>
2015-09-07 16:01:20 -0700

    Fix photo browser and tagger bugs

--- html/assets/script.js
+++ html/assets/script.js
@@ -1,32 +1,52 @@
 $(function() {
-  var adjustPhotos = function() {
-    var images = Math.floor($(window).width() / 320);
-    var width = Math.floor($(window).width() / images);
-    var height = (213 / 320) * width;
-    $(img).width(width);
-    $(img).height(height);
-  };
-  $(window).resize(adjustPhotos);
-  adjustPhotos();
+  var _width = null;
+  var $browser = $('.photo-browser');
 
-  var tagged = {};
-  var tagging = false;
-  var writeTags = function() {
-    $('.photo-browser-tags textarea')[0].textContent = Object.keys(tagged).join('\n') + '\n';
-  };
-  $('.photo-browser-tags button').on('click', function(el) {
-    tagging = !tagging;
-    $(el).closest('.photo-browser').toggleClass('photo-browser-tagging', tagging);
-  });
-  $('img').live('click', function(img) {
-    if (!tagging) {
-      return;
-    }
-    var photo = event.currentTarget.attributes['x-base'].value;
-    var $thumbnail = $(img).closest('.photo-browser-thumbnail');
-    tagged[photo] = !!tagged[photo];
-    $thumbnail.toggleClass('photo-browser-tagged', tagged[photo]);
-    writeTags();
-    return false;
-  });
+  if ($browser.length) {
+    var adjustPhotos = function() {
+      var images = Math.floor($(window).width() / 320);
+      var width = Math.floor($(window).width() / images);
+      if (_width === width) {
+        return;
+      }
+      _width = width;
+      var height = (213 / 320) * width;
+      $('img').width(width);
+      $('img').height(height);
+    };
+    setInterval(adjustPhotos, 1000);
+    $(window).resize(adjustPhotos);
+    adjustPhotos();
+
+    var tagged = {};
+    var tagging = false;
+    var $tags = $('.photo-browser-tags textarea');
+    var writeTags = function() {
+      $tags.text(Object.keys(tagged).join('\n') + '\n');
+    };
+    $tags.focus(function() {
+      $(this).select();
+      $(this).mouseup(function() {
+        $(this).unbind('mouseup');
+        return false;
+      }.bind(this));
+    });
+    $('.photo-browser-tags button').on('click', function(el) {
+      tagging = !tagging;
+      $browser.toggleClass('photo-browser-tagging', tagging);
+    });
+    $browser.on('click', 'img', function(event) {
+      if (!tagging) {
+        return;
+      }
+
+      var $photo = $(this);
+      var $thumbnail = $photo.closest('.photo-browser-thumbnail');
+      var photo = event.currentTarget.attributes['x-base'].value;
+      tagged[photo] = !tagged[photo];
+      $thumbnail.toggleClass('photo-browser-tagged', tagged[photo]);
+      writeTags();
+      return false;
+    });
+  }
 });
