91f86fdb25ba71f27f1a422654ca889f65e53bf2
Primary Oncall <oncall@smyte.com>
2015-09-07 15:41:43 -0700

    Clean photo browser script/css

--- html/assets/script.js
+++ html/assets/script.js
@@ -1,9 +1,32 @@
 $(function() {
-  $('.photo-browser-tags button').on('click', function() {
-    $('img').on('click', function(img) {
-      var photo = event.currentTarget.attributes['x-base'].value;
-      $('.photo-browser-tags textarea')[0].textContent += photo + '\n';
-      return false;
-    });
+  var adjustPhotos = function() {
+    var images = Math.floor($(window).width() / 320);
+    var width = Math.floor($(window).width() / images);
+    var height = (213 / 320) * width;
+    $(img).width(width);
+    $(img).height(height);
+  };
+  $(window).resize(adjustPhotos);
+  adjustPhotos();
+
+  var tagged = {};
+  var tagging = false;
+  var writeTags = function() {
+    $('.photo-browser-tags textarea')[0].textContent = Object.keys(tagged).join('\n') + '\n';
+  };
+  $('.photo-browser-tags button').on('click', function(el) {
+    tagging = !tagging;
+    $(el).closest('.photo-browser').toggleClass('photo-browser-tagging', tagging);
+  });
+  $('img').live('click', function(img) {
+    if (!tagging) {
+      return;
+    }
+    var photo = event.currentTarget.attributes['x-base'].value;
+    var $thumbnail = $(img).closest('.photo-browser-thumbnail');
+    tagged[photo] = !!tagged[photo];
+    $thumbnail.toggleClass('photo-browser-tagged', tagged[photo]);
+    writeTags();
+    return false;
   });
 });
